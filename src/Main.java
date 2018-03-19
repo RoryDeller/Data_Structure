70d07aba38aed179bcc746477855b60648ca6853
61672 <rdeller.61672@farnborough.ac.uk>
2018-03-19 10:29:17 +0000

    Finished.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ public class Main {
         private static String[] list = new String [100];
         private static int listLength = 0;
         private static void add(String item){
-            list[listLength] = item;
-            listLength++;
+            if (listLength == 100) {
+                System.out.println("List is already full");
+                return;
+            } else {
+                list[listLength++] = item;
+            }
         }
 
         private static String get(int index){
-            return list[index];
+            if (index >= listLength){
+                System.out.println("Index is bigger than list size");
+                return "";
+            } else {
+                return list[index];
+            }
         }
 
         private static void clearList(){
@@ -20,16 +29,48 @@ public class Main {
 
         private static String[] stack = new String[100];
         private static int stackHeight = 0;
-        private static void push(String item){}
-        private static void pop(){}
-        private static void clearStack(){}
+        private static void push(String item){
+            if (stackHeight >= 100){
+                System.out.println("Stack is already full");
+            } else {
+                stack[stackHeight++] = item;
+            }
+        }
+        private static String pop(){
+            if(stackHeight == 0){
+                System.out.println("Stack is already empty");
+                return "";
+            } else {
+                return stack[--stackHeight];
+            }
+        }
+        private static void clearStack(){
+            stackHeight = 0;
+        }
 
         private static String[] queue = new String[100];
         private static int queueStart= 0;
         private static int queueLength = 0;
-        private static void enqueue(String item){}
-        //private static String dequeue(){}
-        private static void clearQueue(){}
+        private static void enqueue(String item){ // Add an item; end of queue
+            if (queueLength == 100) {
+                System.out.println("Queue is already full");
+            }    else {
+                queue[(queueStart + queueLength++) % 100] = item;
+            }
+        }
+        private static String dequeue(){ // Remove and item end of queue
+            if(queueLength ==0) {
+            System.out.println("Queue is already empty");
+            return "";
+            }else {
+                queueLength--;
+                return queue[queueStart++];
+            }
+        }
+        private static void clearQueue(){
+            queueLength = 0;
+            queueStart = 0;
+        }
 
         public static void main(String[] args) {
             add("Red");
